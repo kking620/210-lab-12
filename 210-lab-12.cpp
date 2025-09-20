7f42d3332d1fbdf47860aa384190b1d64e9ce85d
Krispy King <kking620@gmail.com>
2025-09-20 16:47:53 -0700

    Fifth commit

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -24,11 +24,42 @@ int main()
         fin.close();
 
         cout << "1. Max Size: " << hourlyWage.size() << endl;
+        
         cout << "2. Values: ";
-        for (double val : hourlyWage) cout << val << " " << endl;
-        cout << "2. Element 4: " << hourlyWage.at(4) << endl;
-        cout << "3. Front: " << hourlyWage.back() << endl;
-        cout << "4. Back: " << hourlyWage.front() << endl;
+        for (double val : hourlyWage) 
+            cout << val << " ";
+
+        cout << endl;
+        
+        cout << "3. Element 4: " << hourlyWage.at(4) << endl;
+        
+        cout << "4. Front: " << hourlyWage.front() << endl;
+        
+        cout << "5. Back: " << hourlyWage.back() << endl;
+
+        cout << "6. Empty? " << (hourlyWage.empty() == 0? "False" : "True") << endl;
+
+        cout << "7. Address: " << hourlyWage.data() << endl;
+
+        sort(hourlyWage.begin(), hourlyWage.end());
+        cout << "8. Sorted: ";
+        for (double val : hourlyWage) 
+            cout << val << " ";
+            
+        cout << endl;
+
+        sort(hourlyWage.rbegin(), hourlyWage.rend());
+        cout << "9. Reverse Sorted: ";
+        for (double val : hourlyWage) 
+            cout << val << " ";
+            
+        cout << endl;
+
+        cout << "10. Max: " << *max_element(hourlyWage.begin(), hourlyWage.end()) << endl;
+
+        cout << "11. Min: " << *min_element(hourlyWage.begin(), hourlyWage.end()) << endl;
+    
+        cout << "12. Sum: " << accumulate(hourlyWage.begin(), hourlyWage.end(), 0) << endl;
     }
     else
         cout << "File not found.\n";
